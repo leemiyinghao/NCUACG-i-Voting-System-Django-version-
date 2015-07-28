c2f57d802423cd268bed210c17b98128b9fd83b6
catLee <leemiyinghao@gmail.com>
2015-07-28 21:21:59 +0800

    Add process with newline in describe and truncate chars in index.

--- Vote/models.py
+++ Vote/models.py
@@ -3,26 +3,28 @@ from django.utils import timezone
 from datetime import timedelta
 
 class VoteableUser(models.Model):
-    userName = models.CharField(max_length=50)
-    nickName = models.CharField(max_length=50)
-    icon = models.ImageField(upload_to = 'usericon', default = '/static/usericon/default.png')
+    verbose_name = 'voteable user'
+    verbose_name_plural = 'voteable user list'
+    userName = models.CharField(max_length=50, verbose_name='user name')
+    nickName = models.CharField(max_length=50, verbose_name='nick name')
+    icon = models.ImageField(upload_to = 'usericon', default = '/static/usericon/default.png', verbose_name='user icon')
 
     def __unicode__(self):
         return self.nickName
 
 class VoteList(models.Model):
-    title = models.CharField(max_length=50)
-    describe = models.TextField(max_length=500)
+    title = models.CharField(max_length=50, verbose_name='vote title')
+    describe = models.TextField(max_length=500, verbose_name='vote describe')
     VOTE_TYPE = (
         ("v", "videoVote"),
         ("s", "selectVote"),
     )
-    voteType = models.CharField(max_length=1, choices=VOTE_TYPE)
-    pubDate = models.DateTimeField('date published', default = timezone.now())
-    expireDate = models.DateTimeField('date expire', default = timezone.now() + timedelta(days = 3))
-    videoURL = models.URLField(null=True)
-    maxSelectCount = models.IntegerField(default=1)
-    videoLength = models.IntegerField(default=0)
+    voteType = models.CharField(max_length=1, choices=VOTE_TYPE, verbose_name='vote type')
+    pubDate = models.DateTimeField(default = timezone.now(), verbose_name='publish date')
+    expireDate = models.DateTimeField(default = timezone.now() + timedelta(days = 3), verbose_name='expire date')
+    videoURL = models.URLField(null=True, verbose_name='video URL')
+    maxSelectCount = models.IntegerField(default=1, verbose_name='max selection')
+    videoLength = models.IntegerField(default=0, verbose_name='video length')
     def hasUserFetchVote(self, _userName):
         if self.fetchvote_set.filter(userName = _userName).count() > 0:
             return True
@@ -32,15 +34,15 @@ class VoteList(models.Model):
         return self.title
 
 class FetchVote(models.Model):
-    userName = models.CharField(max_length=50)
+    userName = models.CharField(max_length=50, verbose_name='user name')
     roomID = models.ForeignKey(VoteList)
-    fetchDate = models.DateTimeField()
+    fetchDate = models.DateTimeField(verbose_name='fetch datetime')
     def __unicode__(self):
         return self.userName
 
 class Options(models.Model):
     roomID = models.ForeignKey(VoteList)
-    text = models.CharField(max_length=500)
+    text = models.CharField(max_length=500, verbose_name='option title')
     def hasUserVoteThisOption(self, _userName):
         return True if self.voteticket_set.filter(mute=False, userName=_userName) else False
     def __unicode__(self):
@@ -48,11 +50,11 @@ class Options(models.Model):
 
 class VoteTicket(models.Model):
     roomID = models.ForeignKey(VoteList)
-    userName = models.CharField(max_length=50)
+    userName = models.CharField(max_length=50, verbose_name='user name')
     score = models.IntegerField(default=0)
     optionID = models.ForeignKey(Options, null=True)
-    doneVideo = models.BooleanField(default=False)
-    mute = models.BooleanField(default=False)
+    doneVideo = models.BooleanField(default=False, verbose_name='has user done the video?')
+    mute = models.BooleanField(default=False, verbose_name='mute')
     def __unicode__(self):
         return self.roomID.title
     def roomVoteType(self):
